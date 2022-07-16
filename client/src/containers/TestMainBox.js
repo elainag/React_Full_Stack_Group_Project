c8ece6169099adfe0a53b00caa48150c7b93165f
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-16 11:53:50 +0100

    question component connected to test container

--- client/src/containers/TestMainBox.js
+++ client/src/containers/TestMainBox.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import Question from "../components/Question";
 
 // container to test components in development
 const TestMainBox = () => {
  return (
     <div className="test-container">
         <h1>Test Container</h1>
+        <Question/>
     </div>
  )
 }
