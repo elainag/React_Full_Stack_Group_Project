b8d565c24a33903a163ac7eb9e40f330c92847e4
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-16 12:51:52 +0100

    generate options added
    app generates 1 correct, 3 random options
    now i'm working on displaying them randomly
    so the correct answer can be in any option

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 const Quiz = () => {
     const [countries, setCountries] = useState([]);
     const [country , setCountry] = useState({});
+    const [question, setQuestion] = useState("");
+    const [options, setOptions] = useState([]);
     const [quiz, setQuiz] = useState({});
 
     useEffect(() => {getCountries()},[])
@@ -12,28 +14,44 @@ const Quiz = () => {
         fetch("https://restcountries.com/v3.1/all")
         .then(result => result.json())
         .then(data => setCountries(data))
-        console.log(countries)
     }
 
     function getRandomIndex() {
         const min = 0;
         const max = countries.length - 1;
         const index = Math.floor(Math.random() * (max - min) + min);
-        console.log(index);
         return index;
     }
 
     function getCountry() {
         const index = getRandomIndex();
         setCountry(countries[index])
-        console.log(country);
     }
+
+    function getQuiz() {
+        generateQuestion();
+        generateOptions();
+        console.log(question);
+        console.log(options);
+    }
+
+    function generateQuestion() {
+        setQuestion(`What is the capital city of ${country.name.official}?`)
+    }
+    function generateOptions() {
+        const answer1 = countries[getRandomIndex()].capital[0];
+        const answer2 = countries[getRandomIndex()].capital[0];
+        const answer3 = countries[getRandomIndex()].capital[0];
+        let generatedOptions = [ answer1, answer2, answer3, country.capital[0]]
+        setOptions(generatedOptions)
+    }
+
 
     return (
         <div>
             {/* <button onClick={get}>Get Quiz</button> */}
             {/* <button onClick={getCountries}>Get Countries</button> */}
-            <button onClick={getCountry}>random index</button>
+            <button onClick={getQuiz}>get quiz</button>
         </div>
     )
 }
