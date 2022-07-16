136c999bb690ba5caa5c22a3e8677e0757fb6cf7
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-16 11:49:06 +0100

    main container for test purposes added, connected to AppJS

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import TestMainBox from './containers/TestMainBox';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
+      <TestMainBox/>
     </div>
   );
 }
