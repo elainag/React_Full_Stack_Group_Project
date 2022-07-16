a977dad21c4941c2a5c7a07dfa70420f9240c78c
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-16 11:49:51 +0100

    containers folder added to commit

---
+++ client/src/containers/TestMainBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+// container to test components in development
+const TestMainBox = () => {
+ return (
+    <div className="test-container">
+        <h1>Test Container</h1>
+    </div>
+ )
+}
+
+export default TestMainBox;
