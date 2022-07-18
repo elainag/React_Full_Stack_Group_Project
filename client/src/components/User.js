92b38aa2a7ac16be63dd34a72ec8425be9b626a0
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-18 16:11:29 +0100

    user component completed, for now it's just a select element show usernames as options

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,7 +1,20 @@
 import React from "react";
 
-const User = () => {
-    return <p>username</p>
+const User = ({ users, onSelectedUser }) => {
+    const userOptions = users.map(user => {
+        return <option key={user._id} value={user._id}>{user.username}</option>
+    })
+    
+    function handleChange(event) {
+        const userID = event.target.value;
+        onSelectedUser(userID);
+    }
+
+    return (
+        <select onChange={handleChange}>
+            {userOptions}
+        </select>
+    )
 }
 
 export default User;
