ca0f3277d2f718560f4f237801e3f48550b64b30
Cetins <69481095+Cetins@users.noreply.github.com>
2022-07-18 15:39:07 +0100

    method names has been updated

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -1,13 +1,13 @@
 const baseURL = 'http://localhost:9000/api/scores'
 
 const UserService = {
-    getScores() {
+    getUsers() {
         return fetch(baseURL)
         .then(res => res.json());
     },
 
-    updateScores(score) {
-        return fetch(baseURL + score._id, {
+    updateUsers(user) {
+        return fetch(baseURL + user._id, {
             method: 'PUT',
             body: JSON.stringify(score),
             headers: {
