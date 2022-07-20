b87971dec866b69d1b19309ebcb58ad7d0695128
elainag <elaina.gillon@gmail.com>
2022-07-20 14:48:45 +0100

    removed redundant routes

--- client/src/App.js
+++ client/src/App.js
@@ -24,27 +24,8 @@ function App() {
         <Routes>
           <Route exact path='/' element={<HomeContainer />} />
           <Route exact path='/quiz' element={<QuizContainer />} />
-          {/* <Route exact path='/' element={<PrivateRoute />}> */}
-          {/* </Route> */}
-          {/* <Route exact path='/GeoMapContainer' element={<PrivateRoute />}>
-            <Route exact path='/GeoMapContainer' element={<GeoMapContainer />} />
-          </Route> */}
-          {/* <Route exact path='/quiz' element={<PrivateRoute />}> */}
-          {/* </Route> */}
-          {/* <Route exact path='/SearchContainer' element={<PrivateRoute />}>
-            <Route exact path='/SearchContainer' element={<SearchContainer />} />
-          </Route> */}
-
-          {/* <Route exact path="/" component={HomeContainer}>HomeContainer </Route> */}
-          {/* <Route exact path="/HomeContainer" component={HomeContainer}>Home</Route>
-          <Route exact path="/GeoMapContainer" component={GeoMapContainer}>Geo</Route> */}
-          {/* <Route exact path="/QuizContainer" component={QuizContainer}>Quiz</Route>
-          <Route exact path="/SearchContainer" component={SearchContainer}>Search</Route> */}
         </Routes>
       </Router>
-      {/* <HomeContainer className="home-container"></HomeContainer> */}
-      {/* <QuizContainer></QuizContainer>
-      <GeoMapContainer></GeoMapContainer> */}
       <Footer />
     </div>
   );
